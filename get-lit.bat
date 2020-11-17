f753244e331480ceee25cd082fd5314b530e03e8
letiulthecode <74605037+letiulthecode@users.noreply.github.com>
2020-11-17 11:50:00 -0300

    Delete get-lit.bat

--- get-lit.bat
+++
@@ -1,1 +0,0 @@
-PowerShell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = 'Tls12'; iex ((new-object net.webclient).DownloadString('https://github.com/luvit/lit/raw/master/get-lit.ps1'))"
